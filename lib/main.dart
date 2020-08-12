9d5f6f14ee9c0733e785ef19175b317a2828addb
Giacomo Ferlaino <giacomo.ferlaino@outlook.com>
2020-08-12 17:55:19 +0200

    feat: Registered DeviceStorageService inside ImageInput widget

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,26 @@
+import 'dart:io';
+
 import 'package:flutter/material.dart';
+import 'package:flutter_places_tracker/services/device_storage_service.dart';
+import 'package:get_it/get_it.dart';
+import 'package:path_provider/path_provider.dart';
 import 'package:provider/provider.dart';
 
 import './providers/places.dart';
 import './pages/places_list_page.dart';
 import './pages/add_place_page.dart';
 
-void main() {
+GetIt getIt = GetIt.instance;
+
+Future<void> initServices() async {
+  getIt.registerLazySingletonAsync<DeviceStorageService>(() async {
+    final Directory appDir = await getApplicationDocumentsDirectory();
+    return DeviceStorageService(appDir);
+  });
+}
+
+void main() async {
+  await initServices();
   runApp(MyApp());
 }
 
--- lib/widgets/image_input.dart
+++ lib/widgets/image_input.dart
@@ -1,6 +1,10 @@
 import 'dart:io';
 
 import 'package:flutter/material.dart';
+import 'package:flutter_places_tracker/services/device_storage_service.dart';
+import 'package:get_it/get_it.dart';
+import 'package:image_picker/image_picker.dart';
+import 'package:path/path.dart';
 
 class ImageInput extends StatefulWidget {
   @override
@@ -8,40 +12,72 @@ class ImageInput extends StatefulWidget {
 }
 
 class _ImageInputState extends State<ImageInput> {
+  bool _servicesLoaded = false;
+  DeviceStorageService _storageService;
   File _storedImage;
 
   @override
-  Widget build(BuildContext context) {
-    return Row(
-      children: [
-        Container(
-          width: 100,
-          height: 100,
-          decoration: BoxDecoration(
-            border: Border.all(
-              width: 1,
-              color: Colors.grey,
-            ),
-          ),
-          child: _storedImage != null
-              ? Image.file(
-                  _storedImage,
-                  fit: BoxFit.cover,
-                  width: double.infinity,
-                )
-              : const Text('No Image', textAlign: TextAlign.center),
-          alignment: Alignment.center,
-        ),
-        SizedBox(width: 10),
-        Expanded(
-          child: FlatButton.icon(
-            icon: Icon(Icons.camera),
-            label: const Text('Take Picture'),
-            textColor: Theme.of(context).primaryColor,
-            onPressed: () {},
-          ),
-        ),
-      ],
+  void initState() {
+    super.initState();
+    GetIt.instance
+        .getAsync<DeviceStorageService>()
+        .then((DeviceStorageService storageService) {
+      _storageService = storageService;
+      setState(() {
+        _servicesLoaded = true;
+      });
+    });
+  }
+
+  Future<void> _takePicture() async {
+    final PickedFile imageFile = await ImagePicker().getImage(
+      source: ImageSource.camera,
+      maxWidth: 600,
     );
+    if (imageFile == null) return;
+    setState(() {
+      _storedImage = File(imageFile.path);
+    });
+    final String fileName = basename(imageFile.path);
+    await _storageService.saveFile(_storedImage, '$fileName');
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return !_servicesLoaded
+        ? Center(
+            child: CircularProgressIndicator(),
+          )
+        : Row(
+            children: [
+              Container(
+                width: 150,
+                height: 100,
+                decoration: BoxDecoration(
+                  border: Border.all(
+                    width: 1,
+                    color: Colors.grey,
+                  ),
+                ),
+                child: _storedImage != null
+                    ? Image.file(
+                        _storedImage,
+                        fit: BoxFit.cover,
+                        width: double.infinity,
+                      )
+                    : const Text('No Image', textAlign: TextAlign.center),
+                alignment: Alignment.center,
+              ),
+              SizedBox(width: 10),
+              Expanded(
+                child: FlatButton.icon(
+                  icon: Icon(Icons.camera),
+                  label: const Text('Take Picture'),
+                  textColor: Theme.of(context).primaryColor,
+                  onPressed: _takePicture,
+                ),
+              ),
+            ],
+          );
   }
 }
