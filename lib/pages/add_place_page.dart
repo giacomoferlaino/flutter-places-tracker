4b79bdfbccc936f555c74073df1bfd29dc49dc56
Giacomo Ferlaino <giacomo.ferlaino@outlook.com>
2020-08-12 13:39:19 +0200

    feat: Added AddPlacePage widget

---
+++ lib/pages/add_place_page.dart
@@ -0,0 +1,52 @@
+import 'package:flutter/material.dart';
+
+import '../widgets/image_input.dart';
+
+class AddPlacePage extends StatefulWidget {
+  static const String routeName = '/add-place';
+
+  @override
+  _AddPlacePageState createState() => _AddPlacePageState();
+}
+
+class _AddPlacePageState extends State<AddPlacePage> {
+  final TextEditingController _titleController = TextEditingController();
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Add a New Place'),
+      ),
+      body: Column(
+        crossAxisAlignment: CrossAxisAlignment.stretch,
+        children: [
+          Expanded(
+            child: SingleChildScrollView(
+              child: Padding(
+                padding: const EdgeInsets.all(10.0),
+                child: Column(
+                  children: [
+                    TextField(
+                      decoration: InputDecoration(labelText: 'Title'),
+                      controller: _titleController,
+                    ),
+                    SizedBox(height: 10),
+                    ImageInput(),
+                  ],
+                ),
+              ),
+            ),
+          ),
+          RaisedButton.icon(
+            icon: Icon(Icons.add),
+            label: Text('Add Place'),
+            onPressed: () {},
+            elevation: 0,
+            materialTapTargetSize: MaterialTapTargetSize.shrinkWrap,
+            color: Theme.of(context).accentColor,
+          ),
+        ],
+      ),
+    );
+  }
+}
